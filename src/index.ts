9f918087aba4b80a53128d4bf6a682f136a10058
Erick Moreno <kreic@live.com>
2022-05-04 11:41:27 -0500

    Updates jest and adds support to aliases

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-import { test } from './mock';
+import { test } from '@utils/placeholderFile';
 
 test();
--- src/test.test.ts
+++ src/test.test.ts
@@ -1,9 +1,10 @@
-import { test } from './mock';
+import { test } from '@utils/placeholderFile';
 
-describe('test()', () => {
+describe('test', () => {
   it('Should be a function', () => {
     const value = test();
 
     expect(value).toBe('Test');
+    expect(test).toBeFunction();
   });
 });
